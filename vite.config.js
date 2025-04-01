66e26f3e3059f565813ef22b2f06ae1a1adbedf1
forte <team@fortetechnologies.io>
2025-04-01 12:14:06 -0400

    mvp commit- not working on safari-with cookies.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server: {
     allowedHosts: ['2ee257c68587.ngrok.app'],
     port: 5175,
